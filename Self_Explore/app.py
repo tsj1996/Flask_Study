8866ae492377742326a272e0e1312c04dfed1e51
Tan, Sijian <stan43@gatech.edu>
2023-03-14 22:53:40 -0400

    Temp Commit

--- Self_Explore/app.py
+++ Self_Explore/app.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#@app.route('/')    
+#def index_2():
+    #return render_template('index_2.html')
+        
+
 #@app.route('/cool_form', methods=['GET', 'POST'])
 @app.route('/index_next.html', methods=['GET', 'POST'])
 #def cool_form():
@@ -26,6 +31,23 @@ def index_next():
     # show the form, it wasn't submitted
     #return render_template('cool_form.html')
     return render_template('index_next.html')
+
+
+@app.route('/index_next_2.html', methods=['GET', 'POST'])
+#def cool_form():
+def index_next_2():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index_2'))
+
+    # show the form, it wasn't submitted
+    #return render_template('cool_form.html')
+    return render_template('index_next_2.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
